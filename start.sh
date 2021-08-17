3fd38c23ac5fec70e2f90702500aba2b4eafa7ef
mrvinil <mrvinil2061@gmail.com>
2021-08-17 08:56:40 +0300

    Final v.3.2

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-CMD gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
-CMD sudo docker-compose exec web python manage.py makemigrations api
-CMD sudo docker-compose exec web python manage.py makemigrations users
-CMD sudo docker-compose exec web python manage.py migrate --noinput
-CMD sudo docker-compose exec web python manage.py collectstatic --no-input
+gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
+sudo docker-compose exec web python manage.py makemigrations api
+sudo docker-compose exec web python manage.py makemigrations users
+sudo docker-compose exec web python manage.py migrate --noinput
+sudo docker-compose exec web python manage.py collectstatic --no-input
