9e5524a948841b8e5672d4f0453b2b3e42576988
mrvinil <mrvinil2061@gmail.com>
2021-08-17 07:54:46 +0300

    Final v.3.1

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
